a248e2ba82f60269cd0e9a2e57e8d078375695ce
bishnunathbd <bishnunath774@gmail.com>
2021-01-17 20:07:08 +0600

    34-7-style shop component using flexbox

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,13 +8,18 @@ const Shop = () => {
   const [products, setProducts] = useState(first10);
 
   return (
-    <div>
-      <h2>Products: {products.length}</h2>
-      <ul>
-        {
-          products.map(product => <li>{product.name}</li>)
-        }
-      </ul>
+    <div className='shop-container'>
+      <div className='product-container'>
+        <h2>Products: {products.length}</h2>
+        <ul>
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ul>
+      </div>
+      <div className='cart-container'>
+          <h2>This is cart</h2>
+      </div>
     </div>
   );
 };
