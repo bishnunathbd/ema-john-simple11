553348b9540040486fbdb90bc5d7804a9ede771c
bishnunathbd <bishnunath774@gmail.com>
2021-01-23 20:32:13 +0600

    37-8-retrieve data from local storage & separate product keys

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
+    console.log(productKeys);
+  }, [])
+
   return (
     <div>
       <h2>This is review</h2>
