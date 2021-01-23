6b98c5baf74d161c01875ad7a82803e54b39bf1c
bishnunathbd <bishnunath774@gmail.com>
2021-01-23 15:53:12 +0600

    37-5-read route parameter by useParams()

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+  const { productKey } = useParams();
+
   return (
     <div>
-      <h1>Product Detail coming soon...</h1>
+      <h1>{productKey} Detail coming soon...</h1>
     </div>
   );
 };
