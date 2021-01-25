7730dcfe1c3ecce6da42d07cc0b56450337b5a6d
bishnunathbd <bishnunath774@gmail.com>
2021-01-25 08:34:24 +0600

    38-4-solved cart NaN problem on addToCart btn click in shop.js

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -12,10 +12,20 @@ const Shop = () => {
   const [cart, setCart] = useState([]);
 
   const handleAddProduct = (product) => {
-    const newCart = [...cart, product];
+    const sameProduct = cart.find(pd => pd.key === product.key);
+    let count = 1;
+    let newCart;
+    if (sameProduct) {
+      count = sameProduct.quantity + 1;
+      sameProduct.quantity = count;
+      const others = cart.filter(pd => pd.key !== sameProduct.key);
+      newCart = [...others, sameProduct];
+    }
+    else {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }
     setCart(newCart);
-    const sameProduct = newCart.filter(pd => pd.key === product.key);
-    const count = sameProduct.length;
     addToDatabaseCart(product.key, count);
   }
 
