a8e94d8c04e3974a33caeb231ff94bc99ffd1d4c
bishnunathbd <bishnunath774@gmail.com>
2021-01-25 10:50:17 +0600

    38-6-place order, clear state & localStorage, show happyImage

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import happyImage from '../../images/giphy.gif';
 
 const Review = () => {
   const [cart, setCart] = useState([]);
+  const [orderPlaced, setOrderPlaced] = useState(false);
+
+  const handlePlaceOrder = () => {
+    setCart([]);
+    setOrderPlaced(true);
+    processOrder();
+  }
+
+  let thankYou;
+  if (orderPlaced) {
+    thankYou = <img src={happyImage} alt=""/>;
+  }
 
   const handleRemoveProduct = (productKey) => {
     const newCart = cart.filter(pd => pd.key !== productKey);
@@ -30,10 +43,13 @@ const Review = () => {
         {
           cart.map(pd => <ReviewItem handleRemoveProduct={handleRemoveProduct} product={pd} key={pd.key}></ReviewItem>)
         }
+        {
+          thankYou
+        }
       </div>
       <div className='cart-container'>
         <Cart cart={cart}>
-          <button className='shop-btn'>Place Order</button>
+          <button onClick={handlePlaceOrder} className='shop-btn'>Place Order</button>
         </Cart>
       </div>
     </div>
